efee639cbf8cdc99885dfed971f03b38e76556af
Ale H <epignosisx@gmail.com>
2015-05-19 22:55:56 -0400

    working on create suite

--- src/Autodash.Core/UI/Models/CreateSuiteVm.cs
+++ src/Autodash.Core/UI/Models/CreateSuiteVm.cs
@@ -8,6 +8,15 @@ namespace Autodash.Core.UI.Models
         public string ProjectId { get; set; }
         public string SuiteName { get; set; }
 
+        public string[] Browsers { get; set; }
+        public string TestTagsQuery { get; set; }
+        public string TestAssembliesPath { get; set; }
+        public TimeSpan TestTimeout { get; set; }
+        public int RetryAttempts { get; set; }
+
+        public TimeSpan Time { get; set; }
+        public TimeSpan Interval { get; set; }
+
         public ValidationFailure[] Errors { get; set; }
 
         public CreateSuiteVm() 
