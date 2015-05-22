baa943b9d966fa2e2dbda8f6a0619e813e349b7b
alejandroh <ahernandezgil@carnival.com>
2015-05-22 08:53:33 -0400

    ensuring mongdb data folder exists

--- tools/run_mongo.bat
+++ tools/run_mongo.bat
@@ -1,1 +1,9 @@
-"c:\Program Files\MongoDB\Server\3.0\bin\mongod.exe" --dbpath c:\temp\mongodb
+set db_path=c:\temp\mongodb\
+
+IF EXIST %db_path% (
+	goto run
+) ELSE (
+	mkdir %db_path%
+)
+:run
+"c:\Program Files\MongoDB\Server\3.0\bin\mongod.exe" --dbpath %db_path%
