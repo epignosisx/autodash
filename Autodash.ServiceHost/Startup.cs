31ef778b6a82b2e80324731b020d0be68344c94c
Alex H <epignosisx@gmail.com>
2015-08-02 22:50:44 -0400

    started work on windows service

---
+++ Autodash.ServiceHost/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+namespace Autodash.ServiceHost
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
